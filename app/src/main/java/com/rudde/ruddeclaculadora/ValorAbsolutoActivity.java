6a5a550459f43201591361bbb56f839dbb19f45b
RomeroSEdgar28 <romero.salyano.edgar@gmail.com>
2021-11-26 08:29:35 -0600

    se agrega calculo de valor absoluto Edgar

--- app/src/main/java/com/rudde/ruddeclaculadora/ValorAbsolutoActivity.java
+++ app/src/main/java/com/rudde/ruddeclaculadora/ValorAbsolutoActivity.java
@@ -3,6 +3,8 @@ package com.rudde.ruddeclaculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ValorAbsolutoActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class ValorAbsolutoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_valor_absoluto);
+    }
+
+    int result, number;
+
+    public void onClickCalcula(View miView){
+        TextView numb = findViewById(R.id.number);
+        number = Integer.parseInt(numb.getText().toString());
+        try{
+            if(number < 0){
+                result = number * (-1);
+            }
+            if(number > 0){
+                result = number;
+            }
+            if(number == 0){
+                result = 0;
+            }
+            numb.setText(String.valueOf(result));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
