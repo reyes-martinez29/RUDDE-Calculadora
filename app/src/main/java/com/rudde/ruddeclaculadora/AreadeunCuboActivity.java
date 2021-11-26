9b985cb3b2966ff547fc7a3f74cb60fbe0072c86
Calculadora1_Actividad1 <mendez.sanchez.ricardo.antonio@gmail.com>
2021-11-26 01:26:06 -0600

    Calcular area de un cubo Ricardo Mendez

--- app/src/main/java/com/rudde/ruddeclaculadora/AreadeunCuboActivity.java
+++ app/src/main/java/com/rudde/ruddeclaculadora/AreadeunCuboActivity.java
@@ -3,12 +3,42 @@ package com.rudde.ruddeclaculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AreadeunCuboActivity extends AppCompatActivity {
+//MENDEZ SANCHEZ RICARDO ANTONIO 5IM9
+    float arista;
+    float resultado;
+    EditText Aristas, Resultados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_areadeun_cubo);
+
+
+        Aristas = findViewById(R.id.aristas);
+        Resultados = findViewById(R.id.resultado);
+        Resultados.setEnabled(false);
+
+
+        Button btn;
+        btn = findViewById(R.id.calcular);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculo();
+            }
+        });
     }
+
+    public void calculo() {
+        arista = Float.parseFloat(Aristas.getText().toString());
+        resultado = (arista*arista)*(6);
+        Resultados.setText(String.valueOf(resultado));
+    }
+
 }
